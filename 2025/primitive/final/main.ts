4ee1f6598edcc3fd17037b78b8c3aa696c0b3717
alex <907785012@qq.com>
2025-08-28 18:19:38 +0800

    complete primitive part

--- 2025/primitive/final/main.ts
+++ 2025/primitive/final/main.ts
@@ -1,10 +1,15 @@
-const justine = {
+type User = {
+  name: string;
+  age: number;
+};
+
+const justine: User = {
   name: 'Justine',
   age: 23,
 };
 
 // Assume we have a user which has an age property
-function isAdult(user) {
+function isAdult(user: User) {
   return user.age >= 18;
 }
 
