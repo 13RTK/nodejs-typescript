ee5f7e7e6b91c90a2038b3c2f43d970ada96fab6
alex <907785012@qq.com>
2025-08-29 22:37:21 +0800

    add starter of runner

---
+++ 2025/runner/starter/main.ts
@@ -0,0 +1,18 @@
+type User = {
+  name: string;
+  age: number;
+};
+
+const justine: User = {
+  name: 'Justine',
+  age: 23,
+};
+
+console.log(justine);
+
+// enum Gender {
+//   Male = 'M',
+//   Female = 'F',
+// }
+
+// console.log(Gender.Male);
